35cbc4ba1d9ab1e0ec65688c443ee44fbaf03b03
woo <woo6794@gmail.com>
2021-01-15 11:50:14 +0900

    [Merge] 개발환경 업데이트

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import * as React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import DashboardContainers from './containers/page/DashboardContainers';
 
-function App() {
-  return (
-    <div className="App"></div>
-  );
-}
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  /* other styles */
+  html, body, #root {
+    height: 100%;
+  }
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+const App = () => (
+  <React.Fragment>
+    <GlobalStyle />
+    <DashboardContainers />
+  </React.Fragment>
+);
 
 export default App;
---
+++ client/src/containers/page/DashboardContainers.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DashboardMain from '../../components/dashboard/DashboardMain';
+
+const DashboardContainers = () => {
+  return <DashboardMain></DashboardMain>;
+};
+
+export default DashboardContainers;
