caa0dbc70466948c283e4370db5896b8e2b6e734
woo <woo6794@gmail.com>
2021-01-20 15:11:34 +0900

    [feature] / 업데이트 DashBoard

--- src/_components/sidebar/SideBar.tsx
+++ src/_components/sidebar/SideBar.tsx
@@ -4,16 +4,40 @@ import styled from 'styled-components';
 
 const Wrap = styled.div`
   display: flex;
+  justify-content: center;
   flex-direction: column;
+  width: 100%;
+`;
+const StyledLink = styled(Link)`
+  color: #f0f0f0;
+  text-decoration: none;
+  font-size: 25px;
+`;
+const LinkWrap = styled.div`
+  padding: 10px;
+  &:hover {
+    background-color: #30323d;
+  }
 `;
 const SideBar = () => {
   return (
     <Wrap>
-      <Link to="/dashboard">dashboard</Link>
-      <Link to="/createroutine">createroutine</Link>
-      <Link to="/workout">workout</Link>
-      <Link to="/usersroutine">usersroutine</Link>
-      <Link to="/mypage">mypage</Link>
+      <LinkWrap>
+        <StyledLink to="/dashboard">dashboard</StyledLink>
+      </LinkWrap>
+      <LinkWrap>
+        <StyledLink to="/createroutine">createroutine</StyledLink>
+      </LinkWrap>
+      <LinkWrap>
+        <StyledLink to="/workout">workout</StyledLink>
+      </LinkWrap>
+      <LinkWrap>
+        <StyledLink to="/usersroutine">usersroutine</StyledLink>
+      </LinkWrap>
+      <LinkWrap>
+        {' '}
+        <StyledLink to="/mypage">mypage</StyledLink>
+      </LinkWrap>
     </Wrap>
   );
 };
