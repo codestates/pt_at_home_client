c7eaccbc123fcface0d5479fee375105698f79a5
woo <woo6794@gmail.com>
2021-01-20 15:40:26 +0900

    [merge] commit for merge

--- src/App.tsx
+++ src/App.tsx
@@ -15,31 +15,66 @@ import {
   UsersRoutine,
   Workout,
 } from './_components/main';
-import DashboardContainers from './containers/page/DashboardContainers';
+import styled from 'styled-components';
 
+const Wrap = styled.div`
+  display: flex;
+  width: 100%;
+`;
 
-const App = ({match, history, location}: RouteComponentProps):JSX.Element => {
-  console.log(location.pathname)
+const Side = styled.div`
+  position: sticky;
+  top: 0;
+  height: 100vh;
+  width: 300px;
+  background-color: #13141c;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 100;
+  box-shadow: 0 1px 30px 0 rgba(2, 2, 3, 0.7);
+`;
 
+const HeaderStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #13141c;
+  box-shadow: 0 1px 30px 0 rgba(2, 2, 3, 0.7);
+  width: 100%;
+`;
+
+const Main = styled.div`
+  width: 100%;
+`;
+
+const App = ({
+  match,
+  history,
+  location,
+}: RouteComponentProps): JSX.Element => {
+  console.log(location.pathname);
   const FeaturePage = () => {
     return (
-      <>
-        <div className="header">
-          <Header />
-        </div>
-        <div className="side-bar">
+      <Wrap>
+        <Side className="side-bar">
           <SideBar />
-        </div>
-        <div className="main">
-          <Switch>
-            <Route path={'/dashboard'} component={DashboardContainers} />
-            <Route path={'/createRoutine'} component={CreateRoutine} />
-            <Route path={'/usersroutine'} component={UsersRoutine} />
-            <Route path={'/workout'} component={Workout} />
-            <Route path={'/mypage'} component={MyPage} />
-          </Switch>
-        </div>
-      </>
+        </Side>
+        <Main className="wrap">
+          <HeaderStyle className="header">
+            <Header />
+          </HeaderStyle>
+
+          <div className="main">
+            <Switch>
+              <Route path={'/dashboard'} component={Dashboard} />
+              <Route path={'/createRoutine'} component={CreateRoutine} />
+              <Route path={'/usersroutine'} component={UsersRoutine} />
+              <Route path={'/workout'} component={Workout} />
+              <Route path={'/mypage'} component={MyPage} />
+            </Switch>
+          </div>
+        </Main>
+      </Wrap>
     );
   };
 
