1d1a1abb1b27b6d1abfd9b0d9234f7ced8fadb97
SEAN LEE <seanlee@MacBook-Pro.local>
2021-02-23 00:25:20 -0600

    update root page style

--- src/components/landingPages/CreateRoutineInfo.tsx
+++ src/components/landingPages/CreateRoutineInfo.tsx
@@ -1,43 +1,77 @@
 import React from 'react';
 import TransitionPageWrap from './TransitionPageWrap';
-import InfoGif from '../../img/create_routine.gif';
+import GifImg from '../../img/create_routine.gif';
 import styled from 'styled-components';
-import Back from '../../img/img5.png';
+import BackImg from '../../img/createroutine.png';
 
 const CreateRoutineInfo = () => {
   return (
-    <TransitionPageWrap backgroundColor={Back}>
+    <TransitionPageWrap backgroundColor={BackImg}>
       <Wrap>
-        <TextWrap>
-        Create Your Own Routine To Run For Today.
-        </TextWrap>
-          <Gif src={InfoGif} />
+        <ContentsWrap>
+          <GifWrap src={GifImg} />
+          <TextWrap>
+            <Text1><TextBold>Create Routine Page </TextBold>에서</Text1>
+            <Text2>저장한 운동으로 나만의 루틴을 만들어보세요.</Text2>
+            <LineBlock></LineBlock>
+          </TextWrap>
+        </ContentsWrap>
       </Wrap>
     </TransitionPageWrap>
   );
 };
 
-
 const Wrap = styled.div`
-  height: 100%;
-  display: flex;
-  flex-flow:column;
-  align-items: center;
-  justify-content: center;
-`;
-const Gif = styled.img`
-  width: 850px;
+  height:100vh;
+  background-image:url(${BackImg})
+`
+const ContentsWrap = styled.div`
+  position:relative;
+  display:flex;
+  flex-flow: column nowrap;
+  width:795px;
+  left:810px;
+  top:200px;
+`
+const TextWrap = styled.div`
+  transform:scale(1, 1.2) translateX(7%)
+
+`
+const TextBold = styled.span`
+  color:rgba(141, 128, 254, 100%);
+  font-weight:bold;
+`
+const Text1 = styled.div`
+  color:#555555;
+  letter-spacing:3px;
+  font-size:2.5rem;
+  margin-top:50px;
+  margin-bottom:20px;
+  padding-left:150px;
+`
+
+const Text2 = styled.div`
+  color:#555555;
+  letter-spacing:3px;
+  font-size:2.3rem;
+  margin-top:20px;
+  margin-bottom:20px;
+  transform:translateX(-10%)
+`
+
+const LineBlock = styled.div`
+  border:2px solid rgba(251, 229, 53, 90%);
+  width:500px;
+  border-radius:10px;
+  margin-bottom:40px;
+  transform:translateX(-17%);
+`
+
+const GifWrap = styled.img`
+  width:750px;
+  htight:395px;
   border-radius:30px;
   box-shadow: -5px -5px 20px #fff, 5px 5px 20px #babecc;
-  margin-left:100px;
-`;
-const TextWrap = styled.div`
-  font-weight:500;
-  color:#b68973;
-  font-size:2.8rem;
-  letter-spacing:3px;
-  line-height:1.5;
-  margin-bottom:70px;
-  margin-right:500px;
-`;
+`
+
 export default CreateRoutineInfo;
--- src/components/landingPages/HomeInfo.tsx
+++ src/components/landingPages/HomeInfo.tsx
@@ -1,93 +1,87 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import Backimg from '../../img/Home.gif';
-import BackImg from '../../img/InfoMain.png';
+import GifImg from '../../img/Home.gif';
+import BackImg from '../../img/home.png';
 const HomeInfo = () => {
   return (
     <Wrap>
       <ContentsWrap>
-        <Contents>
-          <TextWrap>
-            <div> We are here to save you. </div>
-            <div>keep going, </div>
-            <div>and keep working out.</div>
-            <div>Just call it, Save Me HomT!</div>
-          </TextWrap>
-          <Img />
-        </Contents>
+        <TextWrap>
+          <Text1><TextBold>SaveMeHomT </TextBold>와 함께</Text1>
+          <Text2>집에서 재밌게 홈트레이닝을 즐기세요!</Text2>
+          <LineBlock></LineBlock>
+        </TextWrap>
+        <GifWrap src={GifImg} />
+
         <BtnsWrap>
-          <DashBtn>
-            <DashLink to="/dashboard">TRY IT!</DashLink>
-          </DashBtn>
+          <TryBtn>
+            <TryLink to='/dashboard'>Try It!</TryLink>
+          </TryBtn>
           <LoginBtn>
-            <LoginLink to="/login">Login</LoginLink>
+            <LoginLink to='/login'>Login</LoginLink>
           </LoginBtn>
         </BtnsWrap>
-
       </ContentsWrap>
     </Wrap>
   );
 };
 
 const Wrap = styled.div`
-  height: 100%;
-  display: flex;
-  justify-content: flex-end;
-  background-image: url(${BackImg});
-  background-repeat: no-repeat;
-  background-position: bottom;
-  background-position-x: left;
-  background-position-y: 255px;
-  background-size: 1087px;
-`;
+  height:100vh;
+  background-image:url(${BackImg})
+`
 const ContentsWrap = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  margin-right:200px;
-  margin-top:140px;
-  // padding: 25px 100px 0 0;
-`;
-
-const Contents = styled.div`
-  display: flex;
-  // padding: 0 200px 0 0px;
-  // margin-bottom: 200px;
-`;
-const TextWrap = styled.div`
+  position:relative;
   display:flex;
-  flex-flow:column;
-  // align-items:flex-start;
-  justify-contents:center;
-  width: 700px;
-  font-size:2.7rem;
-  line-height:1.5;
-  padding-bottom:80px;
-  font-weight:500;
-  letter-spacing:2px;
-  color:#94b5c0;
-  @media (max-width: 1700px) {
-    font-size : 2.3rem;
-    width : 500px;
-  }
-  @media (max-width: 1575px) {
-    display : none;
-  }
-`;
+  flex-flow: column nowrap;
+  width:795px;
+  left:800px;
+  top:100px;
+`
+const TextWrap = styled.div`
+  transform:scale(1, 1.2) translateX(-10%)
 
-const Img = styled.div`
-  width: 750px;
-  height: 395px;
-  background: url(${Backimg});
+`
+const TextBold = styled.span`
+  color:rgba(232, 102, 96, 100%);
+  font-weight:bold;
+`
+const Text1 = styled.div`
+  color:#555555;
+  letter-spacing:3px;
+  font-size:2.5rem;
+  margin-top:20px;
+  margin-bottom:20px;
+`
+
+const Text2 = styled.div`
+  color:#555555;
+  letter-spacing:3px;
+  font-size:2.5rem;
+  margin-top:20px;
+  margin-bottom:20px;
+  padding-left:75px;
+`
+
+const LineBlock = styled.div`
+  border:2px solid rgba(232, 102, 96, 70%);
+  width:500px;
+  margin-left:350px;
+  border-radius:10px;
+  margin-bottom:40px;
+`
+
+const GifWrap = styled.img`
+  width:750px;
+  htight:395px;
   border-radius:30px;
   box-shadow: -5px -5px 20px #fff, 5px 5px 20px #babecc;
-`;
-
+`
 
 const BtnsWrap = styled.div`
   display:flex;
-  height:200px;
+  justify-content:center;
 `
 
 const LoginBtn = styled.button`
@@ -98,9 +92,10 @@ const LoginBtn = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  border: 1px solid rgba(255, 255, 255, 0.2);
+  // border: 1px solid rgba(255, 255, 255, 0.2);
+  border: 1px solid #30475e;
   border-radius: 20px;
-  background-color: #d3e0dc;
+  background-color: #f6f6f6;
   box-shadow: -5px -5px 20px #fff, 5px 5px 20px #babecc;
   transition: all 0.2s ease-in-out;
   cursor: pointer;
@@ -112,25 +107,23 @@ const LoginBtn = styled.button`
   }
 `;
 const LoginLink = styled(Link)`
-  color: #555555;
+  color: #30475e;
   text-decoration: none;
   width: 100%;
   font-size:1.3rem;
   letter-spacing:3px;
 `;
-const DashBtn = styled.button`
-  font-size:1.3rem;
+const TryBtn = styled.button`
   margin:20px 5px;
   height:45px;
   width:150px;
-  letter-spacing:3px;
   outline: none;
   display: flex;
   justify-content: center;
   align-items: center;
   border: 1px solid rgba(255, 255, 255, 0.2);
   border-radius: 30px;
-  background-color: #aee1e1;
+  background-color: #30475e;
   box-shadow: -5px -5px 20px #fff, 5px 5px 20px #babecc;
   transition: all 0.2s ease-in-out;
   // margin-right: 1200px;
@@ -142,12 +135,14 @@ const DashBtn = styled.button`
     box-shadow: inset 1px 1px 2px #babecc, inset -1px -1px 2px #ffffff;
   }
 `;
-const DashLink = styled(Link)`
-  color:#555555;
+const TryLink = styled(Link)`
+  color:#f5f5f5;
   text-decoration: none;
   width: 100%;
   padding: 0 20px;
   display: block;
+  font-size:1.3rem;
+  letter-spacing:3px;
 `;
 
 export default HomeInfo;
